50087b63bdebfceba7d26dc1d686a93cba2d9c14
Icdan <li215@live.nl>
2018-02-15 14:25:37 +0100

    Kerstboom

--- index.php
+++ index.php
@@ -1,18 +1,11 @@
+<center>
 <?php
-    $leeftijd = 10;
-    $five = "De reis kost 5 euro";
-    $ten = "De reis kost 10 euro";
-    $zero = "De reis is gratis";
+    $row = 15;
 
-    if ($leeftijd > 65) {
-        echo $five;
-    }
-    else if ($leeftijd <= 12 && $leeftijd > 2) {
-        echo $five;
-    }
-    else if ($leeftijd <= 2 ) {
-        echo $zero;
-    } else {
-        echo $ten;
+    for($i = 0; $i < $row; $i++) {
+        for ($j = 0; $j < $i; $j++) {
+            echo "*";
+        }
+        echo "<br>";
     }
 ?>
