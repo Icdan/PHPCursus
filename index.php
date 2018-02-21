c0a5d2ea5165188ba832be99268f7ef64b91f77b
Icdan <li215@live.nl>
2018-02-21 14:42:30 +0100

    Zwemclubs

--- index.php
+++ index.php
@@ -1,19 +1,8 @@
 <?php
-echo "<table>
-            <tr>
-                <td>De spartelkuikens</td>
-                <td>25</td>
-            </tr>
-            <tr>
-                <td>De spartelkuikens</td>
-                <td>25</td>
-            </tr><tr>
-                <td>De spartelkuikens</td>
-                <td>25</td>
-            </tr><tr>
-                <td>De spartelkuikens</td>
-                <td>25</td>
-            </tr>
-        </table>"
-//https://d30y9cdsu7xlg0.cloudfront.net/png/684-200.png
+//print("De volgende momenten zijn nog beschikbaar:<ul>");
+//    foreach($kappersagenda as ..... => .....) {
+//    if($afspraak == "") {
+//    print("<li>".$tijd."</li>") ;
+//    }
+//}
 ?>
--- zwemclubs.php
+++ zwemclubs.php
@@ -1,3 +1,14 @@
 <?php
+$swimimage = "<img style='width:30px; margin-bottom:-10px;' src='https:d30y9cdsu7xlg0.cloudfront.net/png/684-200.png'>";
+$zwemclub["De spartelkuikens"] = 25;
+$zwemclub["De waterbuffels"] = 32;
+$zwemclub["Plonsmderin"] = 11;
+$zwemclub["Bommetje"] = 23;
+foreach ($zwemclub as $names => $numbers) {
+    echo "$names $numbers";
+    for ($i = 0; $i < round(floor($numbers/5), 0, PHP_ROUND_HALF_DOWN); $i++) {
+        echo  $swimimage;
 
+    }   echo "<br>";
+}
 ?>
